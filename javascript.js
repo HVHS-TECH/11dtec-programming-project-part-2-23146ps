353dad87361393f76746e23dec13f80b8fe315b9
23146ps <23146ps@hvhs.school.nz>
2025-06-05 01:29:33 +0000

    rob is dead

--- javascript.js
+++ javascript.js
@@ -5,7 +5,7 @@
 */
 console.log("Running javascript.js");
 
-//Variables
+//functions
 function getUserName() {
     userName = prompt("please input name:");
 }
@@ -15,39 +15,57 @@ function getUserSeatN() {
 function getUserTime() {
     userTime = prompt("Hi "+userName+ " Please input amount of days you wish to rent the vehicle (max of 30):");
 }
-let classArray = ["BAC Mono", "MX5", "Mini", "Falcon", "Outlander", "Estima"];
+function pickCar() {
+        if (userSeatN == 1){
+        alert("You will be renting " + classArray[0] + " for " + userTime + " Day(s). \nYour total price will be $" + 30 * userTime);
+
+    } else if(userSeatN == 2){
+        alert("You will be renting " + classArray[1] + " for " + userTime + " Day(s). \nYour total price will be $" + 50 * userTime);
+
+    } else if(userSeatN == 3 || userSeatN == 4){
+        alert("You will be renting " + classArray[2] + " for " + userTime + " Day(s). \nYour total price will be $" + 70 * userTime);
+
+    } else if(userSeatN == 5){
+        alert("You will be renting " + classArray[3] + " for " + userTime + " Day(s). \nYour total price will be $" + 90 * userTime);
+
+    } else if(userSeatN == 6 || userSeatN == 7){
+        alert("You will be renting " + classArray[4] + " for " + userTime + " Day(s). \nYour total price will be $" + 110 * userTime);
+
+    } else if(userSeatN == 8){
+        alert("You will be renting " + classArray[5] + " for " + userTime + " Day(s). \nYour total price will be $" + 130 * userTime);
+
+    } else {
+        alert("We were unable to proccess your request, please try again.")
+    }
+}
 
 /*
 Main code
 */
 
-
+// constants
+const classArray = ["BAC Mono", "MX5", "Mini", "Falcon", "Outlander", "Estima"];
 const MIN_USER_SEAT_N = 1;
 const MAX_USER_SEAT_N = 8;
 const MIN_USER_TIME = 1;
 const MAX_USER_TIME = 30;
+
+// button function
 function start() {
+
     getUserName();
-    if (!isNaN(userName)|| userName == null || userName == "" || userName == " "){
-        alert("Name must be a valid name")
+    while (!isNaN(userName)|| userName == null || userName == "" || userName == " "){
+        userName = prompt("Name must be a valid name, please enter a valid name");
     } 
     getUserSeatN();
-    if (isNaN(userSeatN)|| userSeatN == null || userSeatN == "" || userSeatN == " " || userSeatN < MIN_USER_SEAT_N || userSeatN > MAX_USER_SEAT_N){
-        alert("amount of seats must be a valid amount of seats")
+    while (isNaN(userSeatN)|| userSeatN == null || userSeatN == "" || userSeatN == " " || userSeatN < MIN_USER_SEAT_N || userSeatN > MAX_USER_SEAT_N){
+        userSeatN = prompt("amount of seats must be a valid amount of seats, plese enter a valid amount of seats");
     }
     getUserTime();
-    if (isNaN(userTime)|| userTime == null || userTime == "" || userTime == " " || userTime < MIN_USER_TIME || userTime > MAX_USER_TIME){
-        alert("Amount of days must be a valid amount of days")
+    while (isNaN(userTime)|| userTime == null || userTime == "" || userTime == " " || userTime < MIN_USER_TIME || userTime > MAX_USER_TIME){
+        userTime = prompt("Amount of days must be a valid amount of days, please enter a valid amount of days");
     }
-    if (userSeatN == 1){
-        alert("You will be renting " + classArray[0] + " for " + userTime + " Days. \nYour total price will be $" + 100 * userTime);
-    } else if(userSeatN == 2){
-        alert("You will be renting " + classArray[1] + " for " + userTime + " Days. \nYour total price will be $" + 200 * userTime);
-    } else if(userSeatN == 3 || userSeatN == 4){
-        alert("You will be renting " + classArray[2] + " for " + userTime + " Days. \nYour total price will be $" + 300 * userTime);
-    } else if(userSeatN == 5){
-        alert("You will be renting " + classArray[1] + " for " + userTime + " Days. \nYour total price will be $" + 400 * userTime);
-    } else if(userSeatN == 6 ||)
+    pickCar();
 }
 /*
 | ||
