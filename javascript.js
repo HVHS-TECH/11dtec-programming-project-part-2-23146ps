0f0139405b86b0325d171901bb923b58c93df9f0
23146ps <23146ps@hvhs.school.nz>
2025-06-06 03:12:30 +0000

    Jun 6, 2025, 3:12 AM

--- javascript.js
+++ javascript.js
@@ -15,29 +15,6 @@ function getUserSeatN() {
 function getUserTime() {
     userTime = prompt("Hi "+userName+ " Please input amount of days you wish to rent the vehicle (max of 30):");
 }
-function pickCar() {
-        if (userSeatN == 1){
-        alert("You will be renting " + classArray[0] + " for " + userTime + " Day(s). \nYour total price will be $" + 30 * userTime);
-
-    } else if(userSeatN == 2){
-        alert("You will be renting " + classArray[1] + " for " + userTime + " Day(s). \nYour total price will be $" + 50 * userTime);
-
-    } else if(userSeatN == 3 || userSeatN == 4){
-        alert("You will be renting " + classArray[2] + " for " + userTime + " Day(s). \nYour total price will be $" + 70 * userTime);
-
-    } else if(userSeatN == 5){
-        alert("You will be renting " + classArray[3] + " for " + userTime + " Day(s). \nYour total price will be $" + 90 * userTime);
-
-    } else if(userSeatN == 6 || userSeatN == 7){
-        alert("You will be renting " + classArray[4] + " for " + userTime + " Day(s). \nYour total price will be $" + 110 * userTime);
-
-    } else if(userSeatN == 8){
-        alert("You will be renting " + classArray[5] + " for " + userTime + " Day(s). \nYour total price will be $" + 130 * userTime);
-
-    } else {
-        alert("We were unable to proccess your request, please try again.")
-    }
-}
 
 /*
 Main code
@@ -45,6 +22,7 @@ Main code
 
 // constants
 const classArray = ["BAC Mono", "MX5", "Mini", "Falcon", "Outlander", "Estima"];
+
 const MIN_USER_SEAT_N = 1;
 const MAX_USER_SEAT_N = 8;
 const MIN_USER_TIME = 1;
