4dcf9416b65cfc40afe050c13b0a220f669b5106
23146ps <23146ps@hvhs.school.nz>
2025-06-25 00:29:41 +0000

    AAAAHHHHH FORM NO WORK

--- javascript.js
+++ javascript.js
@@ -10,23 +10,35 @@ console.log("Running javascript.js");
 Main code
 */
 
-// constants
-const classArray = ["Basic", "Basic+", "Premium", "Premium+", "Suite", "Penthouse"];
-const moons = ["Europa","Io","Callisto","Ganymede"]
+// constants & variables
 const moonMulti = ["2","2.5","1.5","3"]
 const price = ["150","175","250","300","500","2000"]
 
 const MIN_USER_TIME = 1;
 const MAX_USER_TIME = 30;
 
+let totalPrice = 0;
+
+//functions
 function getFormInput(){
     var message ="welcome to the form, ";
     message = message + input_name.value;
     welcomeMessage.innerHTML = message;
 }
 
+function priceCheck(){
+    var price = moon.value * room.value;
 
-//functions
+    var message2 ="Total price: "
+    message2 = message2 + price
+    price.innerHTML = message2;
+}
+/*
+main chunk of codearidooo
+*/
+
+
+
 
 /*
 | ||
